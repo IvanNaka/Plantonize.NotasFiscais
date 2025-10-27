e57c91f9c7402064f5c89ccf780981789b8042e4
Ivan Nakatani <77029720+IvanNaka@users.noreply.github.com>
2025-10-27 14:37:16 -0300

    mongo db

--- Plantonize.NotasFiscais.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Plantonize.NotasFiscais.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -35,6 +36,13 @@ namespace Plantonize.NotasFiscais.Infrastructure.Extensions
             services.AddScoped<IFaturaRepository, FaturaRepository>();
             services.AddScoped<IMunicipioAliquotaRepository, MunicipioAliquotaRepository>();
             services.AddScoped<IImpostoResumoRepository, ImpostoResumoRepository>();
+
+            // Additional configuration for TLS cipher suites
+            configuration.Bind("WEBSITE_TLS_CIPHER_SUITES", new List<string>()
+            {
+                "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+                "TLS_ECDHE_RSA_WITH_AES_128_GLM_SHA256"
+            });
 
             return services;
         }
--- Plantonize.NotasFiscais.Infrastructure/NotasFiscaisDBContext.cs
+++ Plantonize.NotasFiscais.Infrastructure/NotasFiscaisDBContext.cs
@@ -1,5 +1,7 @@
 ﻿using MongoDB.Driver;
 using Plantonize.NotasFiscais.Infrastructure.Configuration;
+using System;
+using System.Security.Authentication;
 
 namespace Plantonize.NotasFiscais.Infrastructure
 {
@@ -11,21 +13,40 @@ namespace Plantonize.NotasFiscais.Infrastructure
         public NotasFiscaisDBContext(MongoDBSettings settings)
         {
             _settings = settings;
-            var client = new MongoClient(settings.ConnectionString);
+
+            // Configure MongoDB client with explicit SSL/TLS settings for Azure compatibility
+            var mongoClientSettings = MongoClientSettings.FromConnectionString(settings.ConnectionString);
+
+            // Configure SSL/TLS settings
+            mongoClientSettings.SslSettings = new SslSettings
+            {
+                EnabledSslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13,
+                CheckCertificateRevocation = false  // Azure App Service compatibility
+            };
+
+            // Increase timeouts for better reliability in cloud environments
+            mongoClientSettings.ServerSelectionTimeout = TimeSpan.FromSeconds(30);
+            mongoClientSettings.ConnectTimeout = TimeSpan.FromSeconds(30);
+            mongoClientSettings.SocketTimeout = TimeSpan.FromSeconds(60);
+
+            // Add retry configuration
+            mongoClientSettings.RetryWrites = true;
+            mongoClientSettings.RetryReads = true;
+
+            var client = new MongoClient(mongoClientSettings);
             _database = client.GetDatabase(settings.DatabaseName);
         }
 
-        public IMongoCollection<NFSEConfiguration> NotasFiscais => 
+        public IMongoCollection<NFSEConfiguration> NotasFiscais =>
             _database.GetCollection<NFSEConfiguration>(_settings.NotasFiscaisCollectionName);
 
-        public IMongoCollection<FaturaConfiguration> Faturas => 
+        public IMongoCollection<FaturaConfiguration> Faturas =>
             _database.GetCollection<FaturaConfiguration>(_settings.FaturasCollectionName);
 
-        public IMongoCollection<MunicipioAliquotaConfiguration> MunicipiosAliquota => 
+        public IMongoCollection<MunicipioAliquotaConfiguration> MunicipiosAliquota =>
             _database.GetCollection<MunicipioAliquotaConfiguration>(_settings.MunicipiosAliquotaCollectionName);
 
-        public IMongoCollection<ImpostoResumoConfiguration> ImpostosResumo => 
+        public IMongoCollection<ImpostoResumoConfiguration> ImpostosResumo =>
             _database.GetCollection<ImpostoResumoConfiguration>(_settings.ImpostosResumoCollectionName);
     }
 }
-
